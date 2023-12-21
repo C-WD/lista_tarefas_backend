354cb4643c897d7bb2f1efc01274ba80d8e74324
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-21 10:01:45 -0300

    Iniciando o Projeto

---
+++ bin/lista_tarefas_back.dart
@@ -0,0 +1,5 @@
+import 'package:lista_tarefas_back/lista_tarefas_back.dart' as lista_tarefas_back;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lista_tarefas_back.calculate()}!');
+}
---
+++ test/lista_tarefas_back_test.dart
@@ -0,0 +1,8 @@
+import 'package:lista_tarefas_back/lista_tarefas_back.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

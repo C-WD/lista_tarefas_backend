e5f2d8d9b3419b81352f8cce445294a2b560c407
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-08 23:28:01 -0300

    Recomeçando projeto hehe

--- bin/lista_tarefas_back.dart
+++
@@ -1,5 +0,0 @@
-import 'package:lista_tarefas_back/lista_tarefas_back.dart' as lista_tarefas_back;
-
-void main(List<String> arguments) {
-  print('Hello world: ${lista_tarefas_back.calculate()}!');
-}
--- test/lista_tarefas_back_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:lista_tarefas_back/lista_tarefas_back.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
